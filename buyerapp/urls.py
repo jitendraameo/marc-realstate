ab6bf8f2086a36e9a4885fcfaca5ca1c70defdb5
jitendra <jitendra5581@gmail.com>
2020-05-05 17:59:39 +0530

    navbar pages added

--- buyerapp/urls.py
+++ buyerapp/urls.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
     path('', home),
-    path('advance_search', advance_search),
+    path('advance-search', advance_search),
+    path('foreclosures', foreclosure_search),
+    path('blog', blog_search),
+    path('about', about_view),
+    path('contact', contact_view),
+    path('market_report', market_view),
 ]
--- buyerapp/views.py
+++ buyerapp/views.py
@@ -5,5 +5,26 @@ from django.http import request
 def home(request):
     return render(request, 'buyer/index.html')   
 
+
 def advance_search(request):
     return render(request, 'buyer/advance-search.html')  
+
+
+def foreclosure_search(request):
+    return render(request, 'buyer/foreclosure.html')  
+
+
+def blog_search(request):
+    return render(request, 'buyer/blog.html')  
+
+
+def about_view(request):
+    return render(request, 'buyer/aboutus.html')  
+        
+
+def contact_view(request):
+    return render(request, 'buyer/contact.html')
+
+
+def market_view(request):
+    return render(request, 'buyer/advance-search.html')
