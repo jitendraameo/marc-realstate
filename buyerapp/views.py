67f68b9647e767787325228d9e7ccfdf7780402c
jitendra <jitendra5581@gmail.com>
2020-05-13 11:57:14 +0530

    webgraphics changed

--- buyerapp/views.py
+++ buyerapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import request
-
+from .models import *
+from django.db.models import Q
 
 def home(request):
-    return render(request, 'buyer/index.html')   
+  
+    return render(request, 'buyer/index.html', {'cities': City.objects.all(), 'areas':Area.objects.all()})   
 
 
 def advance_search(request):
@@ -30,7 +32,45 @@ def market_view(request):
     return render(request, 'buyer/advance-search.html')
 
 def property_search_view(request):
+    # try:
+		city = request.GET['CityChoice']
+		area = request.GET['AreaChoice']
+		lowprice = request.GET['LimitFrom']
+		upprice = request.GET['LimitTo']
 
-    return render(request, 'buyer/property.html')
+		if city and area and lowprice and upprice:
+			property_list = Property.objects.filter((Q(city__exact=city) and Q(area__exact=area))).filter(Q(price__range=(lowprice, upprice)))
+			return render(request, 'buyer/property2.html', {'properties': property_list})
+		elif city and area and lowprice:
+			property_list = Property.objects.filter((Q(city__exact=city) and Q(area__exact=area))).filter(Q(price__gte=lowprice))
+			return render(request, 'buyer/property2.html', {'properties': property_list})
+		elif city and area and upprice:
+			property_list = Property.objects.filter((Q(city__exact=city) and Q(area__exact=area))).filter(Q(price__lte=upprice))
+			return render(request, 'buyer/property2.html', {'properties': property_list})
+		elif city and area:
+			property_list = Property.objects.filter(Q(city__exact=city) and Q(area__exact=area))
+			return render(request, 'buyer/property2.html', {'properties': property_list})
+		elif city and lowprice:
+			property_list = Property.objects.filter(city__name__exact=city).filter(price__gte=lowprice)
+			return render(request, 'buyer/property2.html', {'properties': property_list})
+		elif city and upprice:
+			property_list = Property.objects.filter(city__name__exact=city).filter(price__lte=upprice)
+			return render(request, 'buyer/property2.html', {'properties': property_list})
+		elif city:
+			property_list = Property.objects.filter(city__name__contains=city)
+			return render(request, 'buyer/property2.html', {'properties': property_list})
+		elif lowprice and upprice:
+			property_list = Property.objects.filter(Q(price__range=(lowprice, upprice)))
+			return render(request, 'buyer/property2.html', {'properties': property_list})
+		elif lowprice:
+			property_list = Property.objects.filter(price__gte=lowprice)
+			return render(request, 'buyer/property2.html', {'properties': property_list})
+		elif upprice:
+			property_list = Property.objects.filter(price__lte=upprice)
+			return render(request, 'buyer/property2.html', {'properties': property_list})
+		else:
+			return render(request, 'buyer/property2.html', {'properties': Property.objects.all()})
+
+    
 
 
